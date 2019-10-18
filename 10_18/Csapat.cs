028ae49e7cb74cb6f8dc15e27f8918893ef0dcef
garaguly.csaba@VEREB.DC <garaguly.csaba@VEREB.DC>
2019-10-18 11:28:18 +0200

    Add project files.

---
+++ 10_18/Csapat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_18
+{
+    class Csapat
+    {
+        public string Nev { get; set; }
+        public List<Jatekos> jatekosok { get; set; }
+
+
+    }
+}
